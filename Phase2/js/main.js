5c599c9849efad93c5ec31d92d9cb9ff919ea2eb
田村公佑 <tamurakosuke@tamurakouyuunoMacBook-Air.local>
2025-10-04 18:53:36 +0900

    251004 Phase2のフォルダツリーの作成

---
+++ Phase2/js/main.js
@@ -0,0 +1,6 @@
+// js/main.js
+import { steps } from "./stepData.js";
+import { renderStep } from "./uiHandler.js";
+
+let index = 0;
+renderStep(steps[index]);
---
+++ Phase2/js/stepData.js
@@ -0,0 +1,7 @@
+// js/stepData.js
+export const steps = [
+    { img: "1.jpg", text: "材料を切る" },
+    { img: "2.jpg", text: "炒める" },
+    { img: "3.jpg", text: "盛り付ける" },
+  ];
+  
